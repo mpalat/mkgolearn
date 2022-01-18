85ea1ab294e2c1a04035bac57af2f44f88c771b1
Manoj Palat <manpalat@in.ibm.com>
2022-01-18 14:37:40 +0530

    for loop variations

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -3,10 +3,37 @@ package main
 import "fmt"
 
 func main() {
+
+	// if
 	if no := 21; no%2 == 0 { // these variable have scope of the if statement blocks
 		fmt.Printf("%d", no)
 	} else {
 		fmt.Printf("else:%d", no)
 	}
 	fmt.Println("")
+
+	// for - variation 1
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Printf("sum is : %v\n", sum)
+
+	// for - 2 - use like while
+	nSum := 1
+	for nSum < 100 {
+		nSum += nSum
+	}
+	fmt.Printf("nSum is : %v\n", nSum)
+
+	//variation 3 - infinite loop
+	x := 1
+	for {
+		if x > 100 {
+			break
+		}
+		x += x
+	}
+	fmt.Printf("x is : %v\n", x)
+
 }
