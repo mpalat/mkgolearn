3c6b4b5dbfde8d32f0abfa0057e49be063093117
Manoj Palat <manpalat@in.ibm.com>
2022-01-18 16:33:06 +0530

    refactor to functions

---
+++ 03-functions/assignment-03.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	choice := getInput()
+	if shouldExit(choice) {
+		return
+	}
+
+	n1, n2 := getTwoInputs()
+	fmt.Printf("result is: %d\n", calculate(choice, n1, n2))
+
+}
+
+func getInput() int {
+	fmt.Println("1.Add")
+	fmt.Println("2.Subtract")
+	fmt.Println("3.Multiply")
+	fmt.Println("4.Divide")
+	fmt.Println("5.Exit")
+	fmt.Println("Enter a Choice:")
+
+	var choice int
+	fmt.Scanf("%d\n", &choice)
+	return choice
+}
+
+func shouldExit(choice int) bool {
+	shouldExit := false
+	if choice == 5 {
+		fmt.Println("Exiting")
+		shouldExit = true
+	} else if choice > 5 {
+		fmt.Println("Invalid")
+		shouldExit = true
+	}
+	return shouldExit
+}
+
+func getTwoInputs() (int, int) {
+	var n1, n2 int
+	fmt.Printf("Enter two nos separated by comma:\n")
+	fmt.Scanf("%d,%d", &n1, &n2)
+	return n1, n2
+}
+
+func calculate(choice, n1, n2 int) int {
+	var result int
+	switch choice {
+	case 1:
+		result = n1 + n2
+	case 2:
+		result = n1 - n2
+	case 3:
+		result = n1 * n2
+	case 4:
+		result = n1 / n2
+	default:
+	}
+	return result
+}
