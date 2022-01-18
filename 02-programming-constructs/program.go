cbaaf552f485798d6aa91d0f658e1a6d00a911d5
Manoj Palat <manpalat@in.ibm.com>
2022-01-18 14:50:24 +0530

    switch

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -36,4 +36,65 @@ func main() {
 	}
 	fmt.Printf("x is : %v\n", x)
 
+	// switch construct
+	/*
+	 rank by score
+	 score 0 - 3 -> "Terrible"
+	 score 4 -7 -> "not bad"
+	 score 8-9 ->Goo
+	 10 -> excellent
+	 otherwise -> invalide
+	*/
+
+	score := 7
+	switch score {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+		fmt.Println("Terrible")
+	case 4:
+	case 5:
+	case 6:
+	case 7:
+		fmt.Println("Not Bad")
+	case 8:
+	case 9:
+		fmt.Println("Good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Invalid")
+	}
+
+	score = 10
+	// multi-label case element
+	switch score {
+	case 0, 1, 2, 3:
+		fmt.Println("Terrible")
+	case 4, 5, 6, 7:
+		fmt.Println("Not Bad")
+	case 8, 9:
+		fmt.Println("Good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Invalid")
+	}
+
+	score = 11
+	// Expression switches - The switch does not have the switch expression
+	switch {
+	case score >= 0 && score < 4:
+		fmt.Println("Terrible")
+	case score >= 4 && score < 8:
+		fmt.Println("Not Bad")
+	case score >= 8 && score < 9:
+		fmt.Println("Good")
+	case score == 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Invalid")
+	}
+
 }
