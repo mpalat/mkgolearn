133f2c830858d29d97f80b217248920c0cd7bc8f
Manoj Palat <manpalat@in.ibm.com>
2022-01-19 15:27:03 +0530

    pointers - init

---
+++ 05-pointer/program.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var no int = 10
+	// var addrNo = &no
+	var addrNo *int = &no
+	// fmt.Println(no, addrNo)
+	fmt.Println(*addrNo, addrNo)
+	fmt.Println("after incrementing")
+	increment(addrNo)
+	fmt.Printf("no:%d, addrNo:%p\n", no, addrNo)
+
+	var n1, n2 = 10, 20
+	fmt.Printf("Before swapping - n1 = %d, n2 = %d\n", n1, n2)
+	swap(&n1, &n2)
+	fmt.Printf("After swapping - n1 = %d, n2 = %d\n", n1, n2)
+}
+
+func increment(addrNo *int) {
+	*addrNo++
+}
+func swap(px, py *int) {
+	tmp := *px
+	*px = *py
+	*py = tmp
+}
