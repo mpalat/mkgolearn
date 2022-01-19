56d09955c9c5b5d5aa5bf4b96b98caf381faa65f
Manoj Palat <manpalat@in.ibm.com>
2022-01-19 14:47:20 +0530

    assignment 5

---
+++ 04-collections/assignment-5.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "Pariatur nisi nostrud id ipsum esse minim velit Lorem eiusmod est reprehenderit sint Esse non proident labore dolore labore nisi dolore dolor proident voluptate minim laborum Ad irure ut Lorem exercitation Ipsum reprehenderit consequat veniam non reprehenderit ut eiusmod magna magna aliquip ut sint Mollit irure fugiat nostrud non excepteur aliquip cillum Labore dolore deserunt irure ea temporConsequat ea quis ipsum esse minim reprehenderit amet Qui do dolor do anim occaecat culpa commodo sunt mollit excepteur laboris tempor Ullamco labore quis culpa laborum sunt Culpa incididunt ad consectetur ut deserunt tempor proident ut Qui quis aliqua fugiat sunt dolore commodo reprehenderit veniam tempor Voluptate sit laboris sunt et do sint ea irure veniam duis laborum Quis irure id officia inNisi velit proident cupidatat laborum velit enim qui deserunt consectetur Ad ea quis veniam cupidatat tempor sunt elit velit Sit anim irure sunt ut exercitation excepteur elit laboris occaecat dolor Duis ad commodo ut quis nisi pariatur anim Fugiat ad id anim velit labore"
+
+	//find the wordsize that occurs the most by size and print the word size and the number of occurrence
+	//Hint : use strings.Split()
+
+	//example output : 4 letters occur the most with 29 occurrences
+
+	var sMap map[int]int = make(map[int]int)
+
+	maxVal := 0
+	maxKey := 0
+	words := strings.Split(str, " ")
+	for _, key := range words {
+		iKey := len(key)
+		if val, exists := sMap[iKey]; exists {
+			val++
+			sMap[iKey] = val
+			if val > maxVal {
+				maxVal = val
+				maxKey = iKey
+			}
+		} else {
+			sMap[iKey] = 1
+			if val > maxVal {
+				maxVal = val
+				maxKey = iKey
+			}
+		}
+	}
+
+	fmt.Printf("words with  length:%d occur most %d number of times\n", maxKey, sMap[maxKey])
+
+}
