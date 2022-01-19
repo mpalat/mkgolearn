45fb632c91fd9cc89cb300a96f098ac2f3e1b41a
Manoj Palat <manpalat@in.ibm.com>
2022-01-19 16:44:35 +0530

    alias of package

--- 06-modularity-demo/main.go
+++ 06-modularity-demo/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"modularity-demo/calculator"
+	calc "modularity-demo/calculator"
 	"modularity-demo/calculator/utils"
 )
 
 func main() {
-	result := calculator.Add(10, 10)
+	result := calc.Add(10, 10)
 	fmt.Println(result)
-	result = calculator.Subtract(10, 10)
+	result = calc.Subtract(10, 10)
 	fmt.Println(result)
-	fmt.Println(calculator.GetOpCounter())
-	fmt.Println(calculator.OpCounter)
+	fmt.Println(calc.GetOpCounter())
+	fmt.Println(calc.OpCounter)
 	fmt.Println(utils.IsPrime(17))
 }
