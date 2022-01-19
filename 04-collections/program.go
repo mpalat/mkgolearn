992d04100d9e47c021c322ba6fa1063e01e314b7
Manoj Palat <manpalat@in.ibm.com>
2022-01-19 12:21:18 +0530

    slices arrays

--- 04-collections/program.go
+++ 04-collections/program.go
@@ -24,4 +24,46 @@ func main() {
 	for idx, val := range newNos {
 		fmt.Printf("newNos[%d] = %d\n", idx, val)
 	}
+
+	// Slices - Internally A pointer to an underlying array - but slice is dynamic
+	fmt.Println("Slices")
+	var products []string = []string{"pen", "pencil", "marker"}
+	fmt.Println(products)
+
+	var prods []string
+	prods = append(prods, "Pen")
+	fmt.Printf("prods cap:%d len:%d\n", cap(prods), len(prods))
+	prods = append(prods, "Pencil", "Marker")
+	fmt.Printf("prods cap:%d len:%d\n", cap(prods), len(prods))
+	prods = append(prods, "Mouse")
+	fmt.Printf("prods cap:%d len:%d\n", cap(prods), len(prods))
+	fmt.Println(prods)
+
+	var prodss []string = make([]string, 0, 10)
+	prodss = append(prodss, "Pen")
+	fmt.Printf("prodss cap:%d len:%d\n", cap(prodss), len(prodss))
+	prodss = append(prodss, "Pencil", "Marker")
+	fmt.Printf("prodss cap:%d len:%d\n", cap(prodss), len(prodss))
+	prodss = append(prodss, "Mouse")
+	fmt.Printf("prodss cap:%d len:%d\n", cap(prodss), len(prodss))
+	fmt.Println(prodss)
+
+	// copying
+	nProds := prods
+	fmt.Println(nProds)
+
+	var prodStartEmpty []string = make([]string, 3, 10)
+	fmt.Println("prodStartEmpty")
+	fmt.Println(prodStartEmpty)
+	prodStartEmpty = append(prodStartEmpty, "Pen")
+	fmt.Printf("prodStartEmpty cap:%d len:%d\n", cap(prodStartEmpty), len(prodStartEmpty))
+	prodStartEmpty = append(prodStartEmpty, "Pencil", "Marker")
+	fmt.Printf("prodStartEmpty cap:%d len:%d\n", cap(prodStartEmpty), len(prodStartEmpty))
+	prodStartEmpty = append(prodStartEmpty, "Mouse")
+	fmt.Printf("prodStartEmpty cap:%d len:%d\n", cap(prodStartEmpty), len(prodStartEmpty))
+	fmt.Println(prodStartEmpty)
+	for idx, val := range prodStartEmpty {
+		fmt.Printf("prodStartEmpty[%d] =%s\n", idx, val)
+	}
+
 }
