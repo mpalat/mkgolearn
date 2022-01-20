56644d29f995ff817124c93805726a851fd02a72
Manoj Palat <manpalat@in.ibm.com>
2022-01-20 10:50:29 +0530

    improv the sol

--- 07-structs/program.go
+++ 07-structs/program.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"structs-demo/models"
+	_ "structs-demo/models"
 )
 
 type Product struct {
@@ -19,7 +19,7 @@ func main() {
 	fmt.Printf("%#v\n", product)
 
 	var prod Product = Product{101, "Pen", 10, 100, "Stationary"}
-	fmt.Printf("%#v\n", prod)
+	// fmt.Printf("%#v\n", prod)
 
 	// var prodPart Product = Product{
 	// 	Id:       101,
@@ -33,10 +33,10 @@ func main() {
 	// struct equality comparison by value and not by reference
 	// prod == prodP
 
-	var prodPtr *Product = &prod
-	fmt.Printf("(*prodPtr).Id = %d\n", (*prodPtr).Id)
-	fmt.Printf("prodPtr.Id = %d\n", prodPtr.Id)
-	fmt.Println(prodPtr)
+	// var prodPtr *Product = &prod
+	// fmt.Printf("(*prodPtr).Id = %d\n", (*prodPtr).Id)
+	// fmt.Printf("prodPtr.Id = %d\n", prodPtr.Id)
+	// fmt.Println(prodPtr)
 
 	// empPtr := &Employee{1, "Ram"}
 	// emp := Employee{1, "Lak"}
@@ -44,12 +44,13 @@ func main() {
 	// fmt.Printf("%#v\n", empPtr)
 	// fmt.Printf("%#v\n", emp)
 
-	emp := models.NewEmployee()
-	fmt.Println(emp)
+	// emp := models.NewEmployee()
+	// fmt.Println(emp)
 
 	reducedCost := ApplyDiscount(&prod, 25.0)
 	fmt.Println("Discounted Price:", reducedCost)
-	fmt.Println(prod)
+	fmt.Println("Prod pointer passed - so prod after making the discount permanent")
+	fmt.Println(ToString(&prod))
 }
 
 // func ToString(product Product) string {
